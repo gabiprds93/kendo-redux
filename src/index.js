fe583c43c726779f049c0190033a10bb87194e02
gabiprds93 <gabiprds.93@gmail.com>
2017-10-26 16:44:11 -0500

    Implementando Index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'redux-zero/react';
+import store from './store';
+import {HashRouter, Switch, Route} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Index = () => (
+    <Provider store={store}>
+        <HashRouter>
+            <Switch>
+                <Route  exact path = "/" component={App}/>
+            </Switch>
+        </HashRouter>
+    </Provider>
+)
+
+ReactDOM.render(<Index />, document.getElementById('root'));
 registerServiceWorker();
