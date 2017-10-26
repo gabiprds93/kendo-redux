5c5b419020861585563e3c1df9735208a5910b10
gabiprds93 <gabiprds.93@gmail.com>
2017-10-26 15:59:51 -0500

    Archivo store

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import createStore from 'redux-zero'
+import {SUSHI} from './data'
+
+const initialState = {
+    food: SUSHI,
+    selectedItem : -1,
+}
+
+const store = createStore(initialState)
+export default store
