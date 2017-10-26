a99e743daed354b6586a2c48f3e7ca2f7d8ea9d2
Maria del Rosario Palomino Begazo <100kj.mr@gmail.com>
2017-10-26 16:47:22 -0700

    shopping_car

---
+++ src/Shopping_car.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { connect } from "redux-zero/react";
+import Edamame from './images/edamame.jpg';
+import { Grid, Row, Col } from 'react-bootstrap';
+//import { addComment} from "./actions";
+import './shopping_car.css';
+
+//const SushiDish = ({ image, price }) => {
+const SushiDish = () => {
+
+    /* <div>
+        <img src={image} className="ys-Img" />
+        <p>1 x {price}</p>
+    </div> */
+    return (
+
+        <li className="ys-selectedProdList" aria-selected="false">
+            <a className="ys-viewSelectedItem"><img width="100" height="100" src={Edamame} /></a>
+            <span className="ys-selectedImagePrice"><span>1</span>x<span>$4.2</span></span>
+        </li>
+
+    );
+}
+
+const YourShoppingCar = () => {
+    let totalPrice = 0;
+    return (
+        <div id="ys-divshoppingcar">
+            <h3>your<br />shopping car</h3>
+            <p className="ys-totalPrice">$0.00</p>
+            <a id="ys-emptycar">empty car</a><a id="ys-checkout">checkout</a>
+        </div>
+    );
+}
+
+//const Shopping_car = ({ sushi }) => {
+const Shopping_car = () => {
+    /*const UserSushi = sushi.map((sushiUser, index) => {
+      return (
+        <SushiDish
+          key={index}
+          image={sushiUser.image}
+          name={sushiUser.price}
+        />
+      );
+    });*/
+
+    return (
+
+        <Grid >
+            <Row id="ys-shopInfo" className="active" >
+                <Col xs={12} md={9}>
+                    <ul id="ys-shopList" className="ys-widget ys-listview">
+                        <SushiDish />
+                    </ul>
+                </Col>
+                <Col xs={12} md={3}>
+                    <YourShoppingCar />
+                </Col>
+            </Row>
+        </Grid>
+    );
+}
+
+//const mapToProps = ({ sushi, selectedSushiIndex }) => ({ sushi, selectedSushiIndex });
+export default Shopping_car;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Shopping_car from './Shopping_car';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Shopping_car />, document.getElementById('root'));
 registerServiceWorker();
