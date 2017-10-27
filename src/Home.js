23939eeafd73f5133e61ee5b398bd6b2dfbb612a
esther <estheralvaro44@gmail.com>
2017-10-27 12:04:55 -0500

    app,home,index

--- src/Home.js
+++ src/Home.js
@@ -4,37 +4,31 @@ import { connect } from 'redux-zero/react';
 import './App.css';
 import { Grid, Row, Col, Thumbnail } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
-import data from './data'
+
 
 const Maps = ({ item, index }) => {
   return (
-    // <Grid>
-    //   <Row>
-    //     <Col sm={3} md={3} lg={3}>
-          <div className="y-aplates">
-            <a className="view-details" href="#/menu/1">
-              <img classname="y-aimg" src={item.image} />
-            </a>
-            <strong className='y-astron'>{item.name}</strong>
-            <span className="y-a-price"><span>$</span><span data-bind="text: price">{item.price}</span></span>
-            <button className="add-to-cart" data-bind="click: addToCart">Add to cart</button>
-          </div>
-        /* </Col>
-      </Row>
-    </Grid> */
+    <div className="y-aplates">
+      <a className="view-details" href="#/menu/1">
+        <img classname="y-aimg" src={item.image} />
+      </a>
+      <strong className='y-astron'>{item.name}</strong>
+      <span className="y-a-price"><span>$</span><span data-bind="text: price">{item.price}</span></span>
+      <button className="add-to-cart" data-bind="click: addToCart"> + Add to cart</button>
+    </div>
   )
 }
 
 const Home = ({ food, selectedItem }) => {
   return (
     <Grid>
-    <div className='y-acont'>
-      {
-        food.map((item, index) => {
-          return <Maps key={index} item={item} index={index} />
-        })
-      }
-    </div>
+        <div className='y-acont'>
+          {
+            food.map((item, index) => {
+              return <Maps key={index} item={item} index={index} />
+            })
+          }
+        </div>
     </Grid>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,40 +4,29 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'redux-zero/react';
 import store from './store';
-import {HashRouter, Switch, Route,NavLink,Redirect} from 'react-router-dom';
-import {Grid, Row, Col} from 'react-bootstrap';
+import { HashRouter, Switch, Route, NavLink, Redirect } from 'react-router-dom';
+import { Grid, Row, Col } from 'react-bootstrap';
 import Home from './Home'
 import Checkout from './checkout'
 import yakiUdon from './images/logo.png';
 
-const Index = () => 
-{
-    return(
-        
-            <Provider store={store}>
-                <HashRouter>
-                <div >
-                <Grid className="y-acontain">
-                <Row className="show-grid">
-                    <Col xs={4} md={4}>
+const Index = ({ food }) => {
+    return (
+        <Provider store={store}>
+            <HashRouter>
+                <div className="y-acontain">
                     <NavLink to={"/"}><img src={yakiUdon} /></NavLink>
-                    </Col>
-                    <Col className="text-right" xs={8} md={8}>
-                        <a className="y-ashop" >Shopping Cart
-                            <span className="y-spanrosa"><span className="y-abtnp" >{}0</span>items</span>
-                        </a>
-                    </Col>
-                </Row>
-                <hr />
-            </Grid> 
+                    <NavLink to={"/"} className="y-ashop" >Shopping Cart
+                            <span className="y-spanrosa"><span className="y-abtnp">0</span>items</span>
+                    </NavLink>
+                    <hr />
                     <Switch>
                         <Route exact path="/" component={Home} />
-                        <Route path="/checkout" component={Checkout} />        
+                        <Route path="/checkout" component={Checkout} />
                     </Switch>
-                    
-        </div>
-                </HashRouter>
-            </Provider>
+                </div>
+            </HashRouter>
+        </Provider>
     )
 }
 
